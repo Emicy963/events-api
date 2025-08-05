e6083c24636fc8e3aa1329c2e83d277b0ca155e0
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-05 03:39:33 +0100

    feat: Create the api view for register user

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    """Registro de usuário"""
+    serializer = UserRegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'user': UserRegistrationSerializer(user).data,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
