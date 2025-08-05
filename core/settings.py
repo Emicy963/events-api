4fed6a38109f143b74ddf23ae6d54e6a0902b5fa
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-05 06:08:20 +0100

    feat: config the signal in app files

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     'apps.cores',
-    'apps.accounts'
+    'apps.accounts.apps.AccountsConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
