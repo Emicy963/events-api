238af979d36d62fdcb8ef8fb3d32affda074bd6c
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-05 03:45:43 +0100

    feat: Create the api view for login

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, LoginSerializer
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -18,4 +18,19 @@ def register(request):
             'refresh': str(refresh),
             'access': str(refresh.access_token),
         }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes(AllowAny)
+def login(request):
+    """Login de usuário"""
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.validated_data['user']
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'user_type': user.user_type,
+        })
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
