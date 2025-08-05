85a4988edfbdc480784a15efa42a54c8e6daee71
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-05 04:41:24 +0100

    feat: Created endpoints

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('profile/', views.UserProfileView.as_view(), name='profile'),
-    path('organizer-profile/', views.OrganizerProfileView.as_view(), name='organizer_profile'),
+    path('profile/', views.user_profile_view, name='profile'),
+    path('organizer-profile/', views.organizer_profile_view, name='organizer_profile'),
+    path('me/', views.current_user_detail, name='current_user'),
 ]
 
