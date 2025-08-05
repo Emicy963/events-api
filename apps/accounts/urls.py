eb6ed11173102ca577d319347cc93d6656421637
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-05 06:17:36 +0100

    feat: Config the token blacklist for logout user

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
 from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout'),
     path('profile/', views.user_profile_view, name='profile'),
     path('organizer-profile/', views.organizer_profile_view, name='organizer_profile'),
     path('me/', views.current_user_detail, name='current_user'),
