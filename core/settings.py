3a6471b1b25bd1f5a9b7e4fcae3a8afb73a3183b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-05 01:37:52 +0100

    feat: config the enviroments variables

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nq!-y1!iy-hj4r6_*_h5f0%$y$&mg!$4!$@^2gp@98(ax69h$='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[]).split(',')
 
 
 # Application definition
