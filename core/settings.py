1d5b3047adbd2c7cacdf582ea8c8207e04f4a9b5
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-05 01:51:47 +0100

    feat: config the static files and media

--- core/settings.py
+++ core/settings.py
@@ -194,6 +194,9 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_FILES_DIRS = [BASE_DIR / 'static']
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
