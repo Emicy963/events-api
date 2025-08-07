66bb89c855c7e74d41752f895d8521aac3f99c74
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-07 23:06:22 +0100

    feat: Create the model for Event Category

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.cores.models import TimestampedModel, StatusChoices
 
-# Create your models here.
+class EventCategory(TimestampedModel):
+    """Categorias de eventos populares em Angola"""
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    icon = models.CharField(max_length=50, blank=True) # Font Awesome class
+
+    class Meta:
+        verbose_name_plural = "Event Categories"
