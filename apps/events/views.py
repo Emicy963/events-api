f0bb0c5f4005aa40312286a2d2b9373868cf7341
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-09 00:04:15 +0100

    feat: Create me methods for get and created the events in API View

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,7 +1,9 @@
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, filters
 from .models import Event
-from .serializers import EventSerializer
+from .serializers import EventCreateSerializer, EventSerializer
 
 class EventListCreateView(generics.ListCreateAPIView):
     """Lista e criação de eventos"""
@@ -21,3 +23,34 @@ class EventListCreateView(generics.ListCreateAPIView):
     search_fields = ["title", "description", "venue_name"]
     ordering_fields = ["start_datetime", "created_at", "views_count"]
 
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return EventCreateSerializer
+        return EventSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(organizer=self.request.user)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Filtro por proximidade (GPS)
+        lat = self.request.query_params.get('lat')
+        lng = self.request.query_params.get('lng')
+        radius = self.request.query_params.get('radius', 50)  # km
+        if lat and lng:
+            point = Point(float(lng), float(lat), srid=4326)
+            queryset = queryset.filter(
+	            location__distance_lte=(point, Distance(km=radius))
+	        ).distance(point).order_by('distance')
+        
+        # Filtro por data
+        date_from = self.reques.query_params.get("date_from")
+        date_to = self.request.query_params.get("date_to")
+
+        if date_from:
+            queryset = queryset.filter(start_datetime__gte=date_from)
+        if date_to:
+            queryset = queryset.filter(start_datetime__lte=date_to)
+        
+        return queryset
