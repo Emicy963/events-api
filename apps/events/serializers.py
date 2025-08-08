c6fef7031f08415d0862699100b1b40f17536cce
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-08 13:53:11 +0100

    feat: Create the serializer for  Event

--- apps/events/serializers.py
+++ apps/events/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from .models import EventCategory
+from .models import Event, EventCategory
 
 
 class EventCategorySerializer(serializers.ModelSerializer):
     class Meta:
         models = EventCategory
         fields = "all"
+
+class EventSerializer(serializers.ModelSerializer):
+    category = EventCategorySerializer(read_only=True)
+    organizer_name = serializers.CharField(source="organizer.get_full_name", read_only=True)
+    tickets_available = serializers.IntegerField(read_only=True)
+    class Meta:
+        model = Event
+        fields = "__all__"
+        read_only_fields = [
+            "organizer",
+            "views_count",
+            "tickets_sold",
+            "revenue",
+        ]
