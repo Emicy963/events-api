a70a8f6bac069ea5a85b7e2bf57c563a90079492
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-09 00:33:26 +0100

    feat: Reformated the project using the black

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -7,4 +7,3 @@ urlpatterns = [
     path("slug:slug/", views.EventDetailView.as_view(), name="event_detail"),
     path("location/search/", views.events_by_location, name="events_by_location"),
 ]
-
