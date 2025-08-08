635b27a37e05e4b4439187a47376bbe9785e8c10
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-08 13:47:52 +0100

    feat: Create a serializer for Event Category

--- apps/events/serializers.py
+++ apps/events/serializers.py
@@ -1,4 +1,8 @@
 from rest_framework import serializers
+from .models import EventCategory
 
 
-
+class EventCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        models = EventCategory
+        fields = "all"
