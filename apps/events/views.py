dff2c90ad7533c808a41a4543f5b20fe4b3e1554
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-08 23:48:31 +0100

    feat: Start creation of API View for create and list events

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, filters
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+class EventListCreateView(generics.ListCreateAPIView):
+    """Lista e criação de eventos"""
+    queryset = Event.objects.filter(status="active").select_related("organizer", "category")
+    serializer_class = EventSerializer
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = [
+        "category",
+        "province",
+        "municipality",
+        "organizer",
+    ]
+    search_fields = ["title", "description", "venue_name"]
+    ordering_fields = ["start_datetime", "created_at", "views_count"]
+
