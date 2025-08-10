3539943e44904f69aa471eb055ad9305803f1454
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-10 21:43:15 +0100

    config the urls

--- core/urls.py
+++ core/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("apps.accounts.urls")),
-    path("api/v1/events", include("apps.events.urls")),
-    path("api/v1/tickets", include("apps.tickets.urls")),
+    path("api/v1/events/", include("apps.events.urls")),
+    path("api/v1/tickets/", include("apps.tickets.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
