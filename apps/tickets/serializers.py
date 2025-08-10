c7f1bea6dc7ea6472bb3acc2e3392313f8f07f4f
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-10 22:43:07 +0100

    feat: Create the serializer for create order

--- apps/tickets/serializers.py
+++ apps/tickets/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import TicketType
+from .models import TicketType, Order
 
 
 class TicketTypeSerializer(serializers.ModelSerializer):
@@ -9,3 +9,9 @@ class TicketTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = TicketType
         fields = "__all__"
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+    ticket_items = serializers.JSONField(write_only=True) # [{"ticket_type_id": 1, "quantity": 2}]
+    class Meta:
+        model = Order
+        fields = ["ticket_items", "buyer_name", "buyer_email", "buyer_phone"]
