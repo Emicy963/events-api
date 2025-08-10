6a2597d9ec5dc7805b04703bd85001174d20cb9d
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-10 21:55:53 +0100

    feat: Create a model for ticket type

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from apps.cores.models import TimestampedModel
 
 class TicketTypeChoices(models.TextChoices):
     FREE = "free", "Gratuito"
     PAID = "paid", "Pago"
     DONATAION = "donation", "Doação"
+
+class TickeType(TimestampedModel):
+    """Tipos de ingresso"""
+    event = models.ForeignKey(
+        "events.Event",
+        on_delete=models.CASCADE,
+        related_name="ticket_types"
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
