2785e0bfb9cf9eedf8462541f7b2cd216ba83286
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-10 21:56:02 +0100

    feat: Create a model for ticket type

--- apps/tickets/models.py
+++ apps/tickets/models.py
@@ -15,3 +15,29 @@ class TickeType(TimestampedModel):
     )
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+
+    # Preço
+    type = models.CharField(
+        max_length=20, choices=TicketTypeChoices.choices
+    )
+    price_aoa = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    price_usd = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+
+    # Disponibilidade
+    quantity_total = models.PositiveIntegerField()
+    quantity_sold = models.PositiveIntegerField(default=0)
+    sale_start = models.DateTimeField()
+    sale_end = models.DateTimeField()
+
+    # Configurações
+    min_quantity_per_order = models.PositiveBigIntegerField(default=1)
+    max_quantity_per_order = models.PositiveIntegerField(default=10)
+    is_active = models.BooleanField(default=True)
