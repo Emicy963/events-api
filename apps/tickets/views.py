d3eb303ecdb2cdb04888b390d74d0e8bb45d1667
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-10 22:54:00 +0100

    feat: Create the method for get queryset

--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,6 +1,17 @@
+from django.utils import timezone
 from rest_framework import generics
+from .models import TicketType
 from .serializers import TicketTypeSerializer
 
 class TicketTypeListView(generics.ListAPIView):
     """Lista tipos de ingresso de um evento"""
     serializer_class = TicketTypeSerializer
+
+    def get_queryset(self):
+        event_id = self.kwargs["event_id"]
+        return TicketType.objects.filter(
+            event_id=event_id,
+            is_active=True,
+            sale_start__lte=timezone.now(),
+            sale_end_gte=timezone.now()
+        )
