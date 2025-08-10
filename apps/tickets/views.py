1d8c9a9e569da2a04edd7f2feccaf3a2f9d013f0
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-10 22:51:03 +0100

    feat: Start creating the TicketTypeList API View

--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import TicketTypeSerializer
 
-# Create your views here.
+class TicketTypeListView(generics.ListAPIView):
+    """Lista tipos de ingresso de um evento"""
+    serializer_class = TicketTypeSerializer
