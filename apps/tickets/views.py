a2abd4ede6d4419f23e1175765e0dd065610f403
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-08-14 12:26:28 +0100

    feat: Finish create order method

--- apps/tickets/views.py
+++ apps/tickets/views.py
@@ -1,8 +1,10 @@
 from django.utils import timezone
 from django.db import transaction
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import TicketType
+from .services import TicketService
 from .serializers import TicketTypeSerializer, OrderCreateSerializer
 
 class TicketTypeListView(generics.ListAPIView):
@@ -25,4 +27,21 @@ def create_order(request):
     if serializer.is_valid():
         try:
             with transaction.atomic():
-                order = Ticke
+                order = TicketService.creator_order(buyer=request.user)
+                event_id = request.data.get("event_id")
+                ticket_items = serializer.validated_data["ticket_items"],
+                buyer_data = {
+                    "name": serializer.validated_data["buyer_name"],
+                    "email": serializer.validated_data["buyer_email"],
+                    "phone": serializer.validated_data["buyer_phone"]
+                }
+                return Response({
+                    "order_id": order.id,
+                    "order_number": order.order_number,
+                    "total": order.total_aoa,
+                    "payment_url": f"/api/payments/{order.id}/process/"
+                }, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
